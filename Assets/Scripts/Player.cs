4faf0fee5915846ecbbc72789c575b59e301abdd
RicoShaiLAD <42455847+RicoShaiLAD@users.noreply.github.com>
2018-08-23 12:25:36 +1000

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpheight = 10f;
+    public Rigidbody rigid;
+    // code to only jump once after leaving the ground
+    private bool isGrounded = true;
+    // OnCollisionEnter is called when this collider/rigidbody has begun touching another rigidbody/collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Scope A
+        // check if w key is pressed
+        //forward
+        if (Input.GetKey(KeyCode.W))
+            rigid.AddForce(Vector3.forward * speed);
+        // {} = Scope.
+        // move forward code extended:
+        // Vector3 forward;
+        // forward.x = 0;
+        // forward.y = 0;
+        // forward.z = 1;
+        // new forward code:
+        //Vector3 forward = new Vector3(0, 0, 1);
+
+        if (Input.GetKey(KeyCode.S))
+            rigid.AddForce(Vector3.back * speed);
+        //right
+        if (Input.GetKey(KeyCode.D))
+            rigid.AddForce(Vector3.right * speed);
+        //left
+        if (Input.GetKey(KeyCode.A))
+            rigid.AddForce(Vector3.left * speed);
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigid.AddForce(Vector3.up * jumpheight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        // Scope B
+        // Declaration | Definition
+        // Vector3 forward;
+        // forward.x = 0;
+        // forward.y = 0;
+        // forward.z = 1;
+    }
+}
---
+++ Assets/Scripts/playercontroller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public float jumpHeight = 10f;
+    public float rayDistance = 1f;
+    public Rigidbody rigid;
+    private bool isGrounded = true;
+
+    void OnDrawGizmos()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * rayDistance);
+    }
+
+    bool IsGrounded()
+    {
+        Ray groundRay = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(groundRay, out hit, rayDistance))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal") * moveSpeed;
+        float inputV = Input.GetAxis("Vertical") * moveSpeed;
+        Vector3 moveDir = new Vector3(inputH, 0f, inputV);
+        Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            force.y = jumpHeight;
+        }
+
+        rigid.velocity = force;
+
+
+        if (moveDir.magnitude > 0)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(moveDir);
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, 0.1f);
+        }
+    }
+}
